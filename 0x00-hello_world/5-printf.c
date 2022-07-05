50a0b1e8921a48b1fdb5bafd3e201ba183ea5bca
0766424663 <nqubekonkosenhleshozi@gmail.com>
2022-07-04 19:31:53 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-*Description : main - Prints out a string to stdout.
-
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
