2e5c3dd0af602ac0874e3ea6b31eeaf8697b338d
0766424663 <nqubekonkosenhleshozi@gmail.com>
2022-08-11 17:06:25 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,21 +1,49 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * free_list - function that frees a list_t list
+ * add_node_end - function that adds a new node at the end of a list_t list
  * @head: input header pointer
- * Return: nothing
+ * @str: Input string value
+ * Return: the address of the new element, or NULL if it failed
  */
 
-void free_list(list_t *head)
+list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *node;
 	list_t *tmp;
 
-	while (head != NULL)
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	node->len = _strlen(str);
+	node->str = strdup(str);
+	node->next = NULL;
+	if (*head == NULL)
+		*head = node;
+	else
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp->str);
-		free(tmp);
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = node;
 	}
+	return (node);
+}
+
+/**
+ * _strlen - returns length of string
+ * @s: character of string
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	while (s[i] != 0)
+		i++;
+	return (i);
 }
